99b62ded2fd1ea10d75a1571c4fe41f48411ad09
Michael Pratt <michael@pratt.im>
2012-07-03 00:01:04 -0400

    Updated acquire script for new acquire

--- unit_tests/acquire.py
+++ unit_tests/acquire.py
@@ -15,8 +15,8 @@ class Succeed(gdb.Breakpoint):
         print "Succeeded"
         return False
 
-break_acquire = Acquire("*0x08001678")
-break_fail = Fail("*0x08001676")
-break_succeed = Succeed("*0x0800167e")
+break_acquire = Acquire("*0x08001690")
+break_fail = Fail("*0x080016a4")
+break_succeed = Succeed("*0x080016a2")
 
 gdb.execute("continue")
