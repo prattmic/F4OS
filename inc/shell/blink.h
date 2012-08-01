8e23d983bfc7b794f7269aca5de64f33c138aac7
Michael Pratt <michael@pratt.im>
2012-08-01 02:20:20 -0400

    "Optimize" LED functions.

--- inc/shell/blink.h
+++ inc/shell/blink.h
@@ -1,4 +1,4 @@
 void blink(int argc, char **argv);
-void blue_led(void) __attribute__((optimize(0)));
-void orange_led(void) __attribute__((optimize(0)));
-void green_led(void) __attribute__((optimize(0)));
+void blue_led(void);
+void orange_led(void);
+void green_led(void);
