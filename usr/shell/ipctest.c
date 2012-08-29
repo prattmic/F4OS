7971e1341e26aa744f8a3ec124955b1fba5abdf7
Michael Pratt <michael@pratt.im>
2012-08-28 22:01:05 -0400

    Tasks inherit resources from parent task.

--- usr/shell/ipctest.c
+++ usr/shell/ipctest.c
@@ -8,8 +8,6 @@
 
 void memreader(void);
 
-resource *test_mem = NULL;
-
 void ipctest(int argc, char **argv) {
     if (argc != 1) {
         printf("Usage: %s\n", argv[0]);
@@ -17,18 +15,15 @@ void ipctest(int argc, char **argv) {
     }
     rd_t memrd = open_shared_mem();
     printf("WRITING MEM.\r\n");
+
     swrite(memrd, "THIS IS A TEST OF SHARED MEMORY REGIONS N STUFF.");
-    test_mem = curr_task->task->resources[memrd];
+
     printf("READING MEM.\r\n");
     new_task(&memreader, 5, 0);
 }
 
 void memreader(void) {
     char buf[16];
-    if(test_mem == NULL) {
-        return;
-    }
-    add_resource(curr_task->task, test_mem);
     rd_t memrd = curr_task->task->top_rd - 1;
 
     read(memrd, buf, 10);
