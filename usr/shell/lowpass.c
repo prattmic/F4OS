c4267e3a0ddf211856b065cf1a5e900858f8918f
Michael Yenik <mgyenik@ncsu.edu>
2012-09-30 23:59:21 -0400

    Now that resources are inherited, hackish things are removed from lowpass_test

--- usr/shell/lowpass.c
+++ usr/shell/lowpass.c
@@ -11,22 +11,21 @@
 /* INSANE defs to make it obvious where the magic numbers come from */
 #define DT_TO_JIFFIES   10000
 #define DELTA_T         .01
-#define CUTOFF_FREQ     75
+#define CUTOFF_FREQ     25
 #define TAU             1/CUTOFF_FREQ
 #define ACC_LP_GAIN     DELTA_T/(TAU+DELTA_T)
 #define G(x)            x*.018
 
 typedef struct accel_data {
-    int8_t this;
     int8_t x;
-    int8_t stuff;
     int8_t y;
-    int8_t sucks;
     int8_t z;
 } accel_data;
 
-accel_data asdf = {0,0,0,0,0,0};
+accel_data asdf = {0,0,0};
 accel_data *data = &asdf;
+rd_t accelrd = 255;
+float theta = 0;
 
 void lowpass_test(int argc, char **argv) {
     if (argc != 1) {
@@ -38,17 +37,13 @@ void lowpass_test(int argc, char **argv) {
             q to quit now, any other key will continue.\r\n");
     if(getc() != 'q') {
         printf("LOL TOO LATE NOW FOOL. HOPE YOU GOT YOUR RESET BUTTON HANDY. SCRUB.\r\n");
+        accelrd = open_discovery_accel();
         new_task(&ghetto_lp, 8, DELTA_T*DT_TO_JIFFIES);
     }
 }
 
 void ghetto_lp(void) {
-    static float theta = 0;
-    static rd_t accelrd = -1;
-    if(accelrd == -1) {
-        accelrd = open_discovery_accel();
-    } 
-    read(accelrd, (char *)data, 6);
-    theta = lowpass(theta, atan2(G(data->z), G(data->y)), ACC_LP_GAIN);
-    printf("Filtered roll: %f\r\n", theta);
+    read(accelrd, (char *)data, 3);
+    theta = lowpass(theta, atan2(G((float)data->z), G((float)data->y)), ACC_LP_GAIN);
+    printf("Filtered roll: %f\r\n", theta*180/3.1415926);
 }
