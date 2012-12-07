ac6c7a7bf28960588304077d27412a9cd4a1d24c
Michael Pratt <michael@pratt.im>
2012-12-07 11:34:16 -0500

    Specify board to build for with BOARD
    When building for stm32f40x, you can now specify BOARD=discovery or
    BOARD=px4 to build for each respective board.  This allows c files in
    board/ to include chip/board.h, which simply defines the oscillator
    frequency in MHz, so that clock() can correctly configure the clock for
    168MHz.

---
+++ chip/stm32f40x/board/px4/include/chip/board.h
@@ -0,0 +1,7 @@
+#ifndef BOARD_BOARD_H_INCLUDED
+#define BOARD_BOARD_H_INCLUDED
+
+/* External oscillator frequency */
+#define STM32_OSC_FREQ  24
+
+#endif
--- chip/stm32f40x/clock.c
+++ chip/stm32f40x/clock.c
@@ -1,11 +1,13 @@
 #include <stdint.h>
 #include <dev/registers.h>
 #include <dev/cortex_m.h>
+#include <chip/board.h>
 #include <kernel/fault.h>
 
 #define HSE_STARTUP_TIMEOUT     (uint16_t) (0x0500)         /* Time out for HSE start up */
 /* PLL Options - See RM0090 Reference Manual pg. 95 */
-#define PLL_M      8            /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
+/* We set PLL_M to the board's defined oscillator frequency */
+#define PLL_M      STM32_OSC_FREQ   /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
 #define PLL_N      336
 #define PLL_P      2            /* SYSCLK = PLL_VCO / PLL_P */
 #define PLL_Q      7            /* USB OTG FS, SDIO and RNG Clock =  PLL_VCO / PLLQ */
