c3c3273a64c70b41680186aa1cdb4e6d987dab1b
Michael Pratt <michael@pratt.im>
2012-12-15 22:50:16 -0500

    Add seperate led driver for PX4

---
+++ chip/stm32f40x/dev/hw/px4_led.c
@@ -0,0 +1,61 @@
+#include <stdint.h>
+#include <dev/registers.h>
+#include <dev/hw/gpio.h>
+
+#include <dev/hw/led.h>
+
+/* GPIO LEDs available - Red, Blue */
+uint32_t leds_avail[] = {14, 15};
+
+const uint32_t num_leds = sizeof(leds_avail)/sizeof(leds_avail[0]);
+
+void init_led(void) {
+    /* Enable Port B Clock
+    * See docs/stm32f4_ref.pdf page 110 for description of RCC_AHB1ENR */
+    *RCC_AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+
+    /* Set LED pins to output
+    * See docs/stm32f4_ref.pdf page 148 for description of GPIOB_MODER */
+
+    /* PB14 */
+    gpio_moder(GPIOB, 14, GPIO_MODER_OUT);
+
+    /* PB15 */
+    gpio_moder(GPIOB, 15, GPIO_MODER_OUT);
+    led_disable(1);
+
+    /* Enable red LED */
+    led_enable(0);
+}
+
+uint8_t led_enable(uint32_t led) {
+    if (led >= num_leds) {
+        return 1;
+    }
+
+    /* PX4 LEDs are active low */
+    *GPIO_ODR(GPIOB) &= ~(GPIO_ODR_PIN(leds_avail[led]));
+
+    return 0;
+}
+
+uint8_t led_disable(uint32_t led) {
+    if (led >= num_leds) {
+        return 1;
+    }
+
+    /* PX4 LEDs are active low */
+    *GPIO_ODR(GPIOB) |= GPIO_ODR_PIN(leds_avail[led]);
+
+    return 0;
+}
+
+uint8_t led_toggle(uint32_t led) {
+    if (led >= num_leds) {
+        return 1;
+    }
+
+    *GPIO_ODR(GPIOB) ^= GPIO_ODR_PIN(leds_avail[led]);
+
+    return 0;
+}
