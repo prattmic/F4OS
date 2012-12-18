710b0b317773d14c9632c53e0fba5dc3623799a9
Michael Pratt <michael@pratt.im>
2012-12-18 12:03:48 -0500

    Let GCC provide sized types, add (u)int64_t.

--- include/stdint.h
+++ include/stdint.h
@@ -1,11 +1,13 @@
 #ifndef STDINT_H_INCLUDED
 #define STDINT_H_INCLUDED
 
-typedef unsigned char   uint8_t;
-typedef unsigned short  uint16_t;
-typedef unsigned long   uint32_t;
-typedef signed char     int8_t;
-typedef short           int16_t;
-typedef int             int32_t;
+typedef __UINT8_TYPE__  uint8_t;
+typedef __UINT16_TYPE__ uint16_t;
+typedef __UINT32_TYPE__ uint32_t;
+typedef __UINT64_TYPE__ uint64_t;
+typedef __INT8_TYPE__   int8_t;
+typedef __INT16_TYPE__  int16_t;
+typedef __INT32_TYPE__  int32_t;
+typedef __INT64_TYPE__  int64_t;
 
 #endif
