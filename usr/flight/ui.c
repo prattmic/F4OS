af0a2da53f34d653a5fc17c586dc5840c51be563
Michael Pratt <michael@pratt.im>
2012-12-30 23:17:09 -0500

    Add Roll, Pitch, Heading
    Heading is not tilt compensated.  Despite the mag and accelerometer
    being oriented in the same direction, "forward" for the mag seems to be
    in its -Y direction, as this is the direction that heading readings
    match an actual compass.

--- usr/flight/ui.c
+++ usr/flight/ui.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <kernel/semaphore.h>
 #include <dev/sensors.h>
 
@@ -16,9 +17,17 @@ void ui(void) {
         memcpy(&readings, &current_sensor_readings, sizeof(struct sensors));
         release(&sensor_semaphore);
 
+        float pitch = atan2(readings.accel.x, sqrtf(readings.accel.y*readings.accel.y + readings.accel.z*readings.accel.z));
+        float roll = atan2(readings.accel.y, readings.accel.z);
+
+        float uncomp_heading = atan2(readings.mag.y, readings.mag.x) * RAD_TO_DEG;
+        uncomp_heading = uncomp_heading > 0 ? uncomp_heading : uncomp_heading + 360;
+
         printf("Accel: \t X: %fg \t Y: %fg \t Z: %fg \r\n", readings.accel.x, readings.accel.y, readings.accel.z);
         printf("Gyro: \t X: %fdps \t Y: %fdps \t Z: %fdps \r\n", readings.gyro.x, readings.gyro.y, readings.gyro.z);
         printf("Mag: \t X: %fG \t Y: %fG \t Z: %fG \r\n", readings.mag.x, readings.mag.y, readings.mag.z);
         printf("Baro: %fmb \t Baro temp: %fC \t MPU temp: %fC \r\n", readings.baro.pressure, readings.baro.temp, readings.temp);
+        printf("Roll: %fdeg \t Pitch: %fdeg \r\n", roll*RAD_TO_DEG, pitch*RAD_TO_DEG);
+        printf("Uncomp Heading: %fdeg \r\n", uncomp_heading);
     }
 }
