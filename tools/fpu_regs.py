bc25c168f9704e2c809c6fa7edcf81b4943fc28e
Michael Pratt <michael@pratt.im>
2012-12-30 21:33:08 -0500

    GDB Python script for viewing Cortex-M4 FPU regs
    In GDB, 'source fpu-regs.py' to add the command 'info fpu-regs'.
    This command behaves just like 'info regs', showing you all registers by
    default, or only one if specified.  At this time, the registers cannot
    be set with this command.

---
+++ tools/fpu_regs.py
@@ -0,0 +1,42 @@
+import gdb
+
+# See ARMv7-M Architecture Reference Manual
+# Section C1.6 for details on the debug registers
+
+class FPU_Regs(gdb.Command):
+    """Print ARMv7-M FPU registers."""
+
+    def __init__(self):
+        super(FPU_Regs, self).__init__("info fpu-regs", gdb.COMMAND_DATA, gdb.COMPLETE_NONE)
+
+    def invoke(self, arg, from_tty):
+        if not arg:
+            #s0-s31
+            for n in range(32):
+                self.print_reg(n)
+            self.print_reg(0, fpscr=True)
+        elif arg == "fpscr":
+            self.print_reg(0, fpscr=True)
+        elif arg >= 's0' and arg <= 's31':
+            self.print_reg(int(arg[1:]))
+        else:
+            print "Invalid register '%s'" % arg
+
+    def print_reg(self, num, fpscr=False):
+        gdb.execute("set $data_reg = 0xE000EDF8")
+
+        if fpscr:
+            # Set debug core register selection register
+            gdb.execute("set *0xE000EDF4 = 0x%x" % (0x21))
+            # Grab result from debug core register data register
+            reg = gdb.parse_and_eval("*((unsigned int *)$data_reg)")
+            print "{0:<15}{1:#x}\t{1:d}".format("fpscr", int(reg))
+        else:
+            # Set debug core register selection register
+            gdb.execute("set *0xE000EDF4 = 0x%x" % (0x40 + num))
+            # Grab result from debug core register data register
+            reg = gdb.parse_and_eval("*((float *)$data_reg)")
+            print "s{0:<14}{1:f}\t(raw {2})".format(num, float(reg), reg.cast(gdb.lookup_type("void").pointer()))
+
+
+FPU_Regs()
