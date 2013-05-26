ff4fbf08c15195228af5df55ea5b0893269e2e5c
Michael Pratt <michael@pratt.im>
2013-05-25 20:34:18 -0700

    Remove header dependency sed magic
    This was added in an age gone by, (I think) for header dependency
    generation for autogenerated headers.  I believe that GCC's -MD depends
    files should work fine for header dependencies.

--- tools/build.mk
+++ tools/build.mk
@@ -6,20 +6,11 @@ obj_prefix ?= $(PREFIX)/
 
 $(obj_prefix)%.o : %.S $(BASE)/include/config/autoconf.h
 	@echo "CC $<" && $(CC) -MD -c $(CFLAGS) $< -o $@
-	$(generate_header_depends)
 
 $(obj_prefix)%.o : %.c $(BASE)/include/config/autoconf.h
 	@echo "CC $<" && $(CC) -MD -c $(CFLAGS) $< -o $@
-	$(generate_header_depends)
 
--include $(addprefix $(obj_prefix), $(SRCS:.S=.P))
--include $(addprefix $(obj_prefix), $(SRCS:.c=.P))
-
-define generate_header_depends
-	@cp $(obj_prefix)$*.d $(obj_prefix)$*.P; \
-		sed -e 's/#.*//' -e 's/^[^:]*: *//' -e 's/ *\\$$//' \
-			-e '/^$$/ d' -e 's/$$/ :/' < $(obj_prefix)$*.d >> $(obj_prefix)$*.P; \
-		rm -f $(obj_prefix)$*.d
-endef
+-include $(addprefix $(obj_prefix), $(SRCS:.S=.d))
+-include $(addprefix $(obj_prefix), $(SRCS:.c=.d))
 
 .FORCE:
