4ff3f5fe28031f4082db6ab53a28df649f397728
Michael Pratt <michael@pratt.im>
2013-06-12 21:23:42 -0700

    sched: Move create_context to bottom of file
    It annoys me in the middle.  Hide it at the bottom.
    
    Change-Id: If8bcf410c31dea61c74ce2c36ecc18a5f74a06a9

--- kernel/sched/sched_new.c
+++ kernel/sched/sched_new.c
@@ -44,6 +44,63 @@ fail:
     panic_print("Could not allocate task with function pointer 0x%x", fptr);
 }
 
+static task_ctrl *create_task(void (*fptr)(void), uint8_t priority, uint32_t period) {
+    task_ctrl *task;
+    uint32_t *memory;
+    static uint32_t pid_source = 1;
+    task = (task_ctrl *) kmalloc(sizeof(task_ctrl));
+    if (task == NULL) {
+        return NULL;
+    }
+
+    memory = (uint32_t *) malloc(STKSIZE*4);
+    if (memory == NULL) {
+        kfree(task);
+        return NULL;
+    }
+
+    task->stack_limit       = memory;
+    task->stack_base        = memory + STKSIZE;
+    task->stack_top         = memory + STKSIZE;
+    task->fptr              = fptr;
+    task->priority          = priority;
+    task->running           = 0;
+    task->abort             = 0;
+
+    task->period            = period;
+    task->ticks_until_wake  = period;
+    task->pid               = pid_source++;
+
+    list_init(&task->runnable_task_list);
+    list_init(&task->periodic_task_list);
+    list_init(&task->free_task_list);
+
+    generic_task_setup(get_task_t(task));
+
+    return task;
+}
+
+static int register_task(task_ctrl *task, int periodic) {
+    /* When task switching, we cannot safely modify the task lists
+     * ourselves, instead we must ask the OS to do so for us. */
+    if (task_switching) {
+        SVC_ARG2(SVC_REGISTER_TASK, task, periodic);
+    }
+    else {
+        svc_register_task(task, periodic);
+    }
+
+    return 0;
+}
+
+void svc_register_task(task_ctrl *task, int periodic) {
+    insert_task(runnable_task_list, task);
+
+    if (periodic) {
+        insert_task(periodic_task_list, task);
+    }
+}
+
 void create_context(task_ctrl* task, void (*lptr)(void)) {
     asm volatile("mov     r5, #0                                           \n\
                   stmdb   %[stack]!, {r5}   /* Empty */                    \n\
@@ -104,62 +161,4 @@ void create_context(task_ctrl* task, void (*lptr)(void)) {
                   :[pc] "r" (task->fptr), [lr] "r" (lptr), [frame] "r" (task->stack_limit)
                   /* Clobber */
                   :"r5");
-}
-
-
-static task_ctrl *create_task(void (*fptr)(void), uint8_t priority, uint32_t period) {
-    task_ctrl *task;
-    uint32_t *memory;
-    static uint32_t pid_source = 1;
-    task = (task_ctrl *) kmalloc(sizeof(task_ctrl));
-    if (task == NULL) {
-        return NULL;
-    }
-
-    memory = (uint32_t *) malloc(STKSIZE*4);
-    if (memory == NULL) {
-        kfree(task);
-        return NULL;
-    }
-
-    task->stack_limit       = memory;
-    task->stack_base        = memory + STKSIZE;
-    task->stack_top         = memory + STKSIZE;
-    task->fptr              = fptr;
-    task->priority          = priority;
-    task->running           = 0;
-    task->abort             = 0;
-
-    task->period            = period;
-    task->ticks_until_wake  = period;
-    task->pid               = pid_source++;
-
-    list_init(&task->runnable_task_list);
-    list_init(&task->periodic_task_list);
-    list_init(&task->free_task_list);
-
-    generic_task_setup(get_task_t(task));
-
-    return task;
-}
-
-static int register_task(task_ctrl *task, int periodic) {
-    /* When task switching, we cannot safely modify the task lists
-     * ourselves, instead we must ask the OS to do so for us. */
-    if (task_switching) {
-        SVC_ARG2(SVC_REGISTER_TASK, task, periodic);
-    }
-    else {
-        svc_register_task(task, periodic);
-    }
-
-    return 0;
-}
-
-void svc_register_task(task_ctrl *task, int periodic) {
-    insert_task(runnable_task_list, task);
-
-    if (periodic) {
-        insert_task(periodic_task_list, task);
-    }
 }
