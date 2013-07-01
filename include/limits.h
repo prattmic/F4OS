9e181a784d6e20b2a991f4e3ed3182de35bd41cd
Michael Pratt <michael@pratt.im>
2013-06-30 18:11:25 -0700

    limits: Use GCC provided values, when available
    Also, fix odd backticks
    
    Change-Id: Ic90992936f2d4b8e622af562fa63cf79fc611c6b

--- include/limits.h
+++ include/limits.h
@@ -3,36 +3,36 @@
 
 #define MB_LEN_MAX    1
 
-#define CHAR_BIT 8
+#define CHAR_BIT __CHAR_BIT__
 
-/* Minimum and maximum values a `signed char' can hold.  */
-#define SCHAR_MIN (-128)
-#define SCHAR_MAX 127
+/* Minimum and maximum values a 'signed char' can hold.  */
+#define SCHAR_MAX __SCHAR_MAX__
+#define SCHAR_MIN (-SCHAR_MAX-1)
 
-/* Maximum value an `unsigned char' can hold.  (Minimum is 0).  */
+/* Maximum value an 'unsigned char' can hold.  (Minimum is 0).  */
 #define UCHAR_MAX 255
 
-/* Minimum and maximum values a `char' can hold.  */
+/* Minimum and maximum values a 'char' can hold.  */
 #define CHAR_MIN 0
 #define CHAR_MAX 255
 
-/* Minimum and maximum values a `signed short int' can hold.  */
-#define SHRT_MIN (-32767-1)
-#define SHRT_MAX 32767
+/* Minimum and maximum values a 'signed short int' can hold.  */
+#define SHRT_MAX __SHRT_MAX__
+#define SHRT_MIN (-SHRT_MAX-1)
 
-/* Maximum value an `unsigned short int' can hold.  (Minimum is 0).  */
-#define USHRT_MAX 65535
+/* Maximum value an 'unsigned short int' can hold.  (Minimum is 0).  */
+#define USHRT_MAX __UINT16_MAX__
 
-/* Minimum and maximum values a `signed int' can hold.  */
-#define INT_MAX 2147483647
+/* Minimum and maximum values a 'signed int' can hold.  */
+#define INT_MAX __INT_MAX__
 #define INT_MIN (-INT_MAX-1)
 
-/* Maximum value an `unsigned int' can hold.  (Minimum is 0).  */
+/* Maximum value an 'unsigned int' can hold.  (Minimum is 0).  */
 #define UINT_MAX (INT_MAX * 2U + 1)
 
-/* Minimum and maximum values a `signed long int' can hold.
+/* Minimum and maximum values a 'signed long int' can hold.
    (Same as `int').  */
-#define LONG_MAX 2147483647L
+#define LONG_MAX __LONG_MAX__
 #define LONG_MIN (-LONG_MAX-1)
 
 /* Maximum value an `unsigned long int' can hold.  (Minimum is 0).  */
