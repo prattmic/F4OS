9546dcadcc4b2d54edab0e875a191e4315e94e27
Michael Yenik <mgyenik@ncsu.edu>
2013-07-11 02:52:05 -0400

    obj: initial obj implementation
    Again taking a page from the Linux kernel, I have added an "obj"
    and an API to interact with it. Similar to a kobj, an obj serves
    as an easy and standard way to make something reference counted
    and named. Each obj also has a "type", which describes how to
    unpack the container from the obj and what to do with the obj
    when its reference count reaches zero. It also has a parent, for
    when it and its containing struct are associated with something else.
    
    Differing from kobj though, an obj also has a void* to "ops".
    These "ops" will make more sense later, but they are essentially
    used when a group of objs all have the same purpose and/or support the
    same set of operations, but all have their own implementations.
    
    It's kind of like the specific "methods" on a bunch of things that
    all implement a common "interface"
    
    Change-Id: I214716d2486e7d0d6853824ef13f77c5b4d21b69

---
+++ include/kernel/obj.h
@@ -0,0 +1,109 @@
+/*
+ * OBJ IMPLEMENTATION - Michael Yenik
+ *
+ * An obj is a "thing", meant to be very abstract and useful when used
+ * with other parts of the kernel. The main features it has are:
+ *
+ * ~ A reference count, so that its lifecycle is well defined and it need
+ * not be meticulously tracked as it passes from queue to queue
+ *
+ * ~ A "type". Not totally in the programming language sense, all objs
+ * reference an obj_type, which describes how this particular type of thing
+ * that the obj is in should be destroyed when it is no longer referenced, what
+ * specific operations (functions) should be used when interacting with this
+ * particular obj's container, and how to unpack the container from an obj
+ * reference.
+ *
+ *
+ * THE OBJ API (should never change)
+ *
+ * void obj_release(struct obj *o)
+ * ~~ decrement obj refcount and call destructor if possible
+ *
+ * void obj_init(struct obj *o)
+ * ~~ used to properly initialize embedded obj in some other struct
+ *
+ * char *obj_get_name(struct obj *o)
+ * ~~ returns name of obj
+ *
+ * void obj_set_name(struct obj *o, char *name)
+ * ~~ sets name of obj
+ *
+ * void obj_get(struct obj *o)
+ * ~~ increments refcount and claims as being used
+ *
+ * void *get_container(struct obj *o)
+ * ~~ gets object container using type's offset
+ *
+ */
+
+#ifndef KERNEL_OBJ_H_INCLUDED
+#define KERNEL_OBJ_H_INCLUDED
+
+#include <stdint.h>
+#include <list.h>
+#include <atomic.h>
+#include <kernel/fault.h> /* kind of hacky, for panic_print in assert */
+
+#define assert_type(obj, __type) if((obj)->type != (__type)) \
+        panic_print("Type assertion failed for object %s!\n", (obj)->name)
+
+struct obj_type;
+
+typedef struct obj {
+    atomic_t        refcount;
+    char            *name;
+    struct obj      *parent;    /* Parent in some sense, often a class */
+    struct obj_type *type;
+    struct list     list;
+    void            *ops;       /* Operations that this obj supports, typed from class */
+} obj_t;
+
+typedef struct obj_type {
+    void (*dtor)(struct obj *);
+    uintptr_t offset;
+} obj_type_t;
+
+#define INIT_OBJ(symbol, obj_name, obj_type, obj_ops, obj_parent) { \
+    .refcount = ATOMIC_INIT(1),  \
+    .name = obj_name,   \
+    .parent = obj_parent,   \
+    .type = obj_type,   \
+    .list = INIT_LIST(symbol.list), \
+    .ops = obj_ops, \
+}
+
+/* Implementations for obj API */
+
+static inline void obj_init(struct obj *o, struct obj_type *type, char *name) {
+    atomic_set(&o->refcount, 1);
+    o->type = type;
+    o->name = name;
+    o->list.next = &o->list;
+    o->list.prev = &o->list;
+}
+
+static inline void obj_set_name(struct obj *o, char *name) {
+    o->name = name;
+}
+
+static inline char *obj_get_name(struct obj *o) {
+    return o->name;
+}
+
+static inline void obj_get(struct obj *o) {
+    atomic_inc(&o->refcount);
+}
+
+static inline void obj_put(struct obj *o) {
+    if(atomic_dec_and_test(&o->refcount)) {
+        o->type->dtor(o);
+    }
+}
+
+static inline void *get_container(struct obj *o) {
+    uintptr_t offset = (uintptr_t)o->type->offset;
+    return (void *)((uintptr_t)o - offset);
+}
+
+#endif
