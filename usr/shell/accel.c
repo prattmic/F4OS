236d1b236183a7ba05a293cd8d5e98bddce99d20
Michael Pratt <michael@pratt.im>
2013-09-04 22:02:36 -0400

    shell: automatically detect accelerometer driver
    Make the accel program automatically detect the name of the
    accelerometer driver.  If there are none, or multiple drivers, present
    an error to the user, listing the drivers if there are multiple.
    
    This may get transformed into a generic device shell program to avoid
    copying this functionality to the gyro, mag, and baro programs.
    
    Change-Id: Ifa6e96aeac5ee7c9f8841daa7e616fa96aade26a

--- usr/shell/accel.c
+++ usr/shell/accel.c
@@ -7,12 +7,44 @@
 #include "app.h"
 
 void accel(int argc, char **argv) {
-    if (argc != 1) {
-        printf("Usage: %s\r\n", argv[0]);
+    const char *driver;
+
+    if (argc != 1 && argc != 2) {
+        printf("Usage: %s [driver]\r\n", argv[0]);
         return;
     }
 
-    struct obj *o = device_get("lis302dl");
+    if (argc == 2) {
+        driver = argv[1];
+    }
+    else {
+        int total;
+
+        /* Hopefully, there is only one accelerometer driver */
+        total = device_list_class(&accel_class, &driver, 1);
+        if (total < 1) {
+            printf("Error: No accelerometer drivers found.\r\n");
+            return;
+        }
+        else if (total > 1) {
+            const char *names[total];
+            /* Now, get all of the drivers */
+            total = device_list_class(&accel_class, names, total);
+
+            printf("Multiple accelerometer drivers found:\r\n");
+            for (int i = 0; i < total; i++) {
+                printf("* %s\r\n", names[i]);
+            }
+
+            printf("Please specify a driver as an argument\r\n");
+
+            return;
+        }
+    }
+
+    printf("Connecting to accelerometer '%s'\r\n", driver);
+
+    struct obj *o = device_get(driver);
     if (!o) {
         printf("Error: unable to find accelerometer.\r\n");
         return;
