b0ed4ccd3ae12891f606270e67d8c7d02a8f7d50
Michael Pratt <michael@pratt.im>
2014-02-08 15:38:27 -0500

    dev: add FDT parsing functions
    Add additional FDT helper functions, with the prefix fdtparse_.  These
    provide additional FDT specific functionality not available from libfdt.
    
    Change-Id: I4012020612bfe965dabf76961cff35cd84e9ba0f

---
+++ dev/fdtparse.c
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2014 F4OS Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <libfdt.h>
+#include <dev/fdtparse.h>
+
+/* Get device tree blob from .dtb section */
+extern void *_dtb_start;
+
+static const void *fdt_blob = &_dtb_start;
+
+const void *fdtparse_get_blob(void) {
+    return fdt_blob;
+}
+
+int fdtparse_alias_offset(const void *fdt, const char *name) {
+    const char *path;
+
+    path = fdt_get_alias(fdt, name);
+    if (!path) {
+        return -FDT_ERR_NOTFOUND;
+    }
+
+    return fdt_path_offset(fdt, path);
+}
+
+int fdtparse_get_int(const void *fdt, int offset, const char *name, int *val) {
+    const struct fdt_property *prop;
+    int len;
+    fdt32_t *cell;
+
+    prop = fdt_get_property(fdt, offset, name, &len);
+    if (len < 0) {
+        return len;
+    }
+    else if (len < sizeof(int)) {
+        return -FDT_ERR_NOTFOUND;
+    }
+
+    cell = (fdt32_t *) prop->data;
+
+    *val = fdt32_to_cpu(cell[0]);
+
+    return 0;
+}
+
+void *fdtparse_get_addr32(const void *fdt, int offset, const char *name) {
+    const struct fdt_property *prop;
+    int len;
+    fdt32_t *cell;
+
+    prop = fdt_get_property(fdt, offset, name, &len);
+    if (len < sizeof(fdt32_t)) {
+        return NULL;
+    }
+
+    cell = (fdt32_t *) prop->data;
+
+    return (void *)fdt32_to_cpu(cell[0]);
+}
+
+int fdtparse_get_gpio(const void *fdt, int offset, const char *name,
+                      struct fdt_gpio *gpio) {
+    const struct fdt_property *prop;
+    int len;
+    fdt32_t *cell;
+
+    prop = fdt_get_property(fdt, offset, name, &len);
+    if (len < 0) {
+        return len;
+    }
+
+    /* GPIO cells have 3 fields */
+    if (len != 3*sizeof(uint32_t)) {
+        return -FDT_ERR_BADLAYOUT;
+    }
+
+    cell = (fdt32_t *) prop->data;
+
+    /* cell[0] is gpio path, cell[1] is number, cell[2] is flags */
+    gpio->gpio = fdt32_to_cpu(cell[1]);
+    gpio->flags = fdt32_to_cpu(cell[2]);
+
+    return 0;
+}
---
+++ include/dev/fdtparse.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2014 F4OS Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef DEV_FDTPARSE_H_INCLUDED
+#define DEV_FDTPARSE_H_INCLUDED
+
+/* Additional helper functions for parsing FDT */
+
+struct fdt_gpio {
+    int gpio;
+    int flags;
+};
+
+/**
+ * Get the global FDT blob
+ *
+ * @returns pointer to the device FDT blob
+ */
+const void *fdtparse_get_blob(void);
+
+/**
+ * Retreive the offset of the node referenced by a given alias
+ *
+ * @param fdt   pointer to the device tree blob
+ * @param name  name of the alias to look up
+ * @returns:
+ *    structure block offset of the node with the requested alias (>=0),
+ *    on success, on error:
+ *    -FDT_ERR_NOTFOUND, if the requested alias or /aliases does not exist
+ *    -FDT_ERR_BADMAGIC,
+ *    -FDT_ERR_BADVERSION,
+ *    -FDT_ERR_BADSTATE,
+ *    -FDT_ERR_BADSTRUCTURE,
+ *    -FDT_ERR_TRUNCATED, standard meanings.
+ */
+int fdtparse_alias_offset(const void *fdt, const char *name);
+
+/**
+ * Extract 32-bit integer from propery
+ *
+ * @param fdt   pointer to the device tree blob
+ * @param offset    offset of node containing desired propery
+ * @param name  string name of register property
+ * @param val   pointer to int to place result in
+ * @returns 0 on success, on error:
+ *    -FDT_ERR_NOTFOUND, if property is not found, or too small
+ *    -FDT_ERR_BADMAGIC,
+ *    -FDT_ERR_BADVERSION,
+ *    -FDT_ERR_BADSTATE,
+ *    -FDT_ERR_BADSTRUCTURE,
+ *    -FDT_ERR_TRUNCATED, standard meanings.
+ */
+int fdtparse_get_int(const void *fdt, int offset, const char *name, int *val);
+
+/**
+ * Extract register 32-bit address from 'regs' property
+ *
+ * Note: the address is expected to be a 32-bit address; 1 address-cell.
+ *
+ * @param fdt   pointer to the device tree blob
+ * @param offset    offset of node containing desired propery
+ * @param name  string name of register property
+ * @returns address stored in property, NULL on error
+ */
+void *fdtparse_get_addr32(const void *fdt, int offset, const char *name);
+
+/**
+ * Extract GPIO information from GPIO cell property
+ *
+ * GPIO cells have the form <&gpio_device gpio_num gpio_flags>.  This function
+ * extracts that information into a struct fdt_gpio.
+ *
+ * @param fdt   pointer to the device tree blob
+ * @param offset    offset of node containing desired propery
+ * @param name  string name of gpio property
+ * @param gpio  pointer to struct fdt_gpio to return value in
+ * @returns 0 on success,
+ *    -FDT_ERR_BADLAYOUT, if the cell property is not 3 values in length
+ *    -FDT_ERR_NOTFOUND, node does not have named property
+ *    -FDT_ERR_BADOFFSET, offset did not point to FDT_BEGIN_NODE tag
+ *    -FDT_ERR_BADMAGIC,
+ *    -FDT_ERR_BADVERSION,
+ *    -FDT_ERR_BADSTATE,
+ *    -FDT_ERR_BADSTRUCTURE,
+ *    -FDT_ERR_TRUNCATED, standard meanings
+ */
+int fdtparse_get_gpio(const void *fdt, int offset, const char *name,
+                      struct fdt_gpio *gpio);
+
+#endif
