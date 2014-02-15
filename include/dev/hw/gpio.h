3411abf2eb3e6a8b616aac399d2a4d3a74894ab0
Michael Pratt <michael@pratt.im>
2014-02-15 15:56:29 -0500

    dev: GPIO FDT flags
    Add an enum of valid flags for an FDT GPIO flags field.  Currently, only
    active low status can be specified.
    
    Change-Id: I196d35955ba192d9976746ec9e74cf838f810911

--- include/dev/hw/gpio.h
+++ include/dev/hw/gpio.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 F4OS Authors
+ * Copyright (C) 2013, 2014 F4OS Authors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -37,6 +37,11 @@ struct gpio {
 static inline struct gpio *to_gpio(struct obj *o) {
     return (struct gpio *) container_of(o, struct gpio, obj);
 }
+
+/* Valid FDT GPIO flags */
+enum gpio_fdt_flags {
+    GPIO_FDT_ACTIVE_LOW = (1 << 0),
+};
 
 enum gpio_direction {
     GPIO_INPUT  = (0 << 0),
