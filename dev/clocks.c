f7d55a9cebf1827c2fd1f52fb7cf46ff458c8271
Michael Pratt <michael@pratt.im>
2014-03-09 23:32:28 -0400

    dev: add clocks interface
    Add a generic clocks interface for enabling and disabling peripheral
    clocks.  This interface take advantage of the clocks property bindings
    in the device tree.  This property contains a phandle to the clock
    controller, plus controller-defined cell values, generally a clock or
    register number.
    
    clocks_enable() and clocks_disable() will look up the referenced clock
    controller, and find its driver from a staticly-defined list of clock
    drivers, passing control to the compatible driver for actual work to be
    done.
    
    An additional parameter setting function can be used to select a source
    for a clock or set the clock speed.
    
    TODO: create general static lists, to avoid adding new linker sections
    and list implementations ad nauseam.
    
    Change-Id: If8dc3f266a85a93073837a92181daee61d228d38

---
+++ dev/clocks.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2014 F4OS Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <libfdt.h>
+#include <stddef.h>
+#include <dev/clocks.h>
+
+extern struct clock_driver _clocks_start;
+extern struct clock_driver _clocks_end;
+
+static struct clock_driver *drivers = &_clocks_start;
+
+#define NUM_DRIVERS ((int)(&_clocks_end - &_clocks_start))
+
+static struct clock_driver *clocks_lookup(const void *fdt, int offset, const char *name) {
+    const struct fdt_property *prop;
+    int len, controller;
+    fdt32_t *cell;
+    uint32_t phandle;
+
+    prop = fdt_get_property(fdt, offset, name, &len);
+    if (len < 0) {
+        return NULL;
+    }
+
+    /* Make sure there is at least one cell */
+    if (len < sizeof(fdt32_t)) {
+        return NULL;
+    }
+
+    /* The first cell is a phandle to the clock controller */
+    cell = (fdt32_t *) prop->data;
+    phandle = fdt32_to_cpu(cell[0]);
+
+    controller = fdt_node_offset_by_phandle(fdt, phandle);
+    if (controller < 0) {
+        return NULL;
+    }
+
+    for (int i = 0; i < NUM_DRIVERS; i++) {
+        if (!fdt_node_check_compatible(fdt, controller, drivers[i].compat)) {
+            return &drivers[i];
+        }
+    }
+
+    /* Not found! */
+    return NULL;
+}
+
+int clocks_enable(const void *fdt, int offset, const char *name) {
+    struct clock_driver *driver = clocks_lookup(fdt, offset, name);
+    if (!driver) {
+        return -1;
+    }
+
+    return driver->enable(fdt, offset, name);
+}
+
+int clocks_disable(const void *fdt, int offset, const char *name) {
+    struct clock_driver *driver = clocks_lookup(fdt, offset, name);
+    if (!driver) {
+        return -1;
+    }
+
+    return driver->disable(fdt, offset, name);
+}
+
+int clocks_set_param(const void *fdt, int offset, const char *name,
+                     unsigned int param) {
+    struct clock_driver *driver = clocks_lookup(fdt, offset, name);
+    if (!driver) {
+        return -1;
+    }
+
+    return driver->set_param(fdt, offset, name, param);
+}
---
+++ include/dev/clocks.h
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2014 F4OS Authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef DEV_CLOCKS_H_INCLUDED
+#define DEV_CLOCKS_H_INCLUDED
+
+/* TODO: Generic static list/array implementation */
+
+struct clock_driver {
+    /* FDT compatible string this driver is compatible with */
+    const char *compat;
+    /*
+     * Enable clocks
+     *
+     * Enable clocks described by the passed clocks property.
+     *
+     * @param fdt   pointer to FDT blob
+     * @param offset    Node offset containing clocks property
+     * @param prop  String name of clocks property
+     * @returns 0 on success, negative on error
+     */
+    int (*enable)(const void *fdt, int offset, const char *prop);
+    /*
+     * Disable clocks
+     *
+     * Disable clocks described by the passed clocks property.
+     *
+     * @param fdt   pointer to FDT blob
+     * @param offset    Node offset containing clocks property
+     * @param prop  String name of clocks property
+     * @returns 0 on success, negative on error
+     */
+    int (*disable)(const void *fdt, int offset, const char *prop);
+    /*
+     * Select clock parameter
+     *
+     * Set a clock parameter for the clock described by the passed
+     * clock-select property.  This is defined by the clock, but is
+     * general either a clock source or speed.
+     *
+     * TODO: Support more than just the first entry.
+     *
+     * @param fdt   pointer to FDT blob
+     * @param offset    Node offset containing clocks property
+     * @param prop  String name of clock-select property
+     * @param param Parameter value to set
+     * @returns 0 on success, negative on error
+     */
+    int (*set_param)(const void *fdt, int offset, const char *name,
+                     unsigned int param);
+};
+
+/*
+ * Declare an available clock driver
+ *
+ * Sets up a struct definition for a clock driver, which should
+ * be assigned to.
+ *
+ * Example:
+ * DECLATE_CLOCK_DRIVER(example) = {
+ *     .compat = "mycorp,example",
+ *     .enable = &example_enable,
+ *     .disable = &example_enable,
+ *     .set_param = &example_set_param,
+ * };
+ */
+#define DECLARE_CLOCK_DRIVER(name) \
+    struct clock_driver name##_clock_driver __attribute__((section(".clocks")))
+
+/*
+ * Enable clocks
+ *
+ * Enable clocks described by the passed clocks property.
+ *
+ * TODO: Enable more than just the first entry.
+ *
+ * @param fdt   pointer to FDT blob
+ * @param offset    Node offset containing clocks property
+ * @param prop  String name of clocks property
+ * @returns 0 on success, negative on error
+ */
+int clocks_enable(const void *fdt, int offset, const char *name);
+
+/*
+ * Disable clocks
+ *
+ * Disable clocks described by the passed clocks property.
+ *
+ * TODO: Enable more than just the first entry.
+ *
+ * @param fdt   pointer to FDT blob
+ * @param offset    Node offset containing clocks property
+ * @param prop  String name of clocks property
+ * @returns 0 on success, negative on error
+ */
+int clocks_disable(const void *fdt, int offset, const char *name);
+
+/*
+ * Select clock parameter
+ *
+ * Set a clock parameter for the clock described by the passed
+ * clock property.  This is defined by the clock, but is
+ * generally either a clock source or speed.
+ *
+ * TODO: Support more than just the first entry.
+ *
+ * @param fdt   pointer to FDT blob
+ * @param offset    Node offset containing clocks property
+ * @param prop  String name of clock-select property
+ * @param param Parameter value to set
+ * @returns 0 on success, negative on error
+ */
+int clocks_set_param(const void *fdt, int offset, const char *name,
+                     unsigned int param);
+
+#endif
