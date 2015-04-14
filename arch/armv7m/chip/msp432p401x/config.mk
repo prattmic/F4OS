8b041e68693a22e8f78ab9fb34931182f72b2019
Michael Pratt <michael@pratt.im>
2015-04-13 22:49:02 -0700

    msp432: add ROM prototypes
    Include rom.h from TI's MSPWare driverlib, to provide easy access to ROM
    functions.
    
    Change-Id: I02e8b91d5b8c549b08f374635323e7148b118b43

--- arch/armv7m/chip/msp432p401x/config.mk
+++ arch/armv7m/chip/msp432p401x/config.mk
@@ -1,2 +1,5 @@
 # Core is a Cortex-M4F
 include $(BASE)/arch/$(CONFIG_ARCH)/chip/cortex-m4f.mk
+
+# For arch/chip/rom.h
+CFLAGS += -DTARGET_IS_MSP432P4XX
--- arch/armv7m/chip/msp432p401x/include/rom.h
+++
@@ -1,1 +0,0 @@
-/* TODO: prototypes for ROM-library functions */
