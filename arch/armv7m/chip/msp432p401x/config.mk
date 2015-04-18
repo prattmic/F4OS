5992e4fd656fd5f0c9bbae7ae987cbd4b83abd8a
Michael Pratt <michael@pratt.im>
2015-04-17 21:11:42 -0700

    msp432: import TI driverlib clock support
    The majority of TI's driverlib clock functions are not available in the
    on-chip ROM, and must instead be built from source.  Import the clock
    source, and necessary support headers.
    
    The imported files are vastly unchanged, except:
    * Includes are updated to allow proper building
    * SysCtl_* function calls were updated to refer to the ROM versions of
      those functions.
    * CS_(un)registerInterrupt() functions were removed, to avoid a
      dependency on the interrupt source files, which would hijack F4OS'
      interrupt handling.
    
    Change-Id: Ie6b85fa031f38eb149c9f88deb9fcd294a208be0

--- arch/armv7m/chip/msp432p401x/config.mk
+++ arch/armv7m/chip/msp432p401x/config.mk
@@ -1,5 +1,5 @@
 # Core is a Cortex-M4F
 include $(BASE)/arch/$(CONFIG_ARCH)/chip/cortex-m4f.mk
 
-# For arch/chip/rom.h
-CFLAGS += -DTARGET_IS_MSP432P4XX
+# For TI driverlib
+CFLAGS += -DTARGET_IS_MSP432P4XX -D__MSP432P401R__
